676a2e6885e00eff6d9bae899a48c49f4b8845bb
tharaka <w.tharakadinesh@cbanewzealand.org.nz>
2020-11-14 02:20:10 +1300

    second level mind map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,36 +4,16 @@ import { Dispositions } from './model/dipositions';
 
 const HIERARCHY_RULES = {
   ROOT: {
-    name: 'XX汽车有限公司',
+    name: 'HIERARCHY_R',
     backgroundColor: '#7EC6E1',
     getChildren: () => [
       HIERARCHY_RULES.SALES_MANAGER,
-      HIERARCHY_RULES.SHOW_ROOM,
-      HIERARCHY_RULES.SALES_TEAM
     ]
   },
   SALES_MANAGER: {
-    name: '销售经理',
+    name: 'SALES_M',
     color: '#fff',
-    backgroundColor: '#616161',
-    getChildren: () => [
-      HIERARCHY_RULES.SHOW_ROOM,
-      HIERARCHY_RULES.SALES_TEAM
-    ]
-  },
-  SHOW_ROOM: {
-    name: '展厅',
-    color: '#fff',
-    backgroundColor: '#989898',
-    getChildren: () => [
-      HIERARCHY_RULES.SALES_TEAM
-    ]
-  },
-  SALES_TEAM: {
-    name: '销售小组',
-    color: '#fff',
-    backgroundColor: '#C6C6C6',
-    getChildren: () => []
+    backgroundColor: '#616161'
   }
 };
 
@@ -79,9 +59,6 @@ let securityPolicy1_2 = {
   children: []
 }
 
-
-
-
 const mainMindMap = {
   format: 'nodeTree',
   data: {
@@ -95,11 +72,12 @@ const mainMindMap = {
       {
         id: 10,
         color: '#fff',
-        topic: 'Users and Organizations',
+        topic: 'Users & Organizations',
         direction: 'right',
         backgroundColor: '#9ff4f7',
         canHandleClick:true,
-        children: []
+        children: [],
+        show:false,
       },
       {
         id: 30,
@@ -112,9 +90,10 @@ const mainMindMap = {
       {
         id: 50,
         color: '#fff',
-        topic: 'System Architecture and Infrastructure',
+        topic: 'System Architecture & Infrastructure',
         direction: 'right',
-        backgroundColor: '#a3cc5a'
+        backgroundColor: '#a3cc5a',
+        children: []
       },
       {
         id: 70,
@@ -161,12 +140,10 @@ export class AppComponent {
   ngOnInit() {
     this.mindMap = MindMapMain.show(option, mainMindMap);
     
-    
     this.mindMap.view.addEvent(this, 'click',function (event) {
-      debugger
+
            const element = event.target || event.srcElement;
            const nodeid = this.mindMap.view.getBindedNodeId(element);
-          
 
            if("1"!=nodeid && null!=nodeid && !this.selectedNodes.includes(+nodeid)){
               this.selectedNodes.push(+nodeid);
@@ -191,6 +168,264 @@ export class AppComponent {
       mainMindMap.data.children.forEach((e, i) =>{
         debugger
         if(e.id == element){
+
+          if(e.id == 10 ){
+              e.children.push({
+                id: 11,
+                color: '#fff',
+                topic: 'Social Issues & Professional Practice',
+                direction: 'right',
+                backgroundColor: '#9ff4f7'
+              },
+              {
+                id: 12,
+                color: '#fff',
+                topic: 'Security Policy & Management',
+                direction: 'right',
+                backgroundColor: '#9ff4f7'
+              },
+              {
+                id: 13,
+                color: '#fff',
+                topic: 'IS Management & Leadership',
+                direction: 'right',
+                backgroundColor: '#9ff4f7'
+              },
+              {
+                id: 14,
+                color: '#fff',
+                topic: 'Enterprise Architecture',
+                direction: 'right',
+                backgroundColor: '#9ff4f7'
+              },
+              {
+                id: 15,
+                color: '#fff',
+                topic: 'Project Management',
+                direction: 'right',
+                backgroundColor: '#9ff4f7'
+              },
+              {
+                id: 16,
+                color: '#fff',
+                topic: 'User Experience Design',
+                direction: 'right',
+                backgroundColor: '#9ff4f7'
+              });
+          }
+          if(e.id ==  30 ){
+              e.children.push({
+                id: 31,
+                color: '#fff',
+                topic: 'Security Issues & Principles',
+                direction: 'right',
+                backgroundColor: '#fde5c6'
+              },
+              {
+                id: 32,
+                color: '#fff',
+                topic: 'System Analysis & Design',
+                direction: 'right',
+                backgroundColor: '#fde5c6'
+              },
+              {
+                id: 33,
+                color: '#fff',
+                topic: 'Requirement Analysis & Design',
+                direction: 'right',
+                backgroundColor: '#fde5c6'
+              },
+              {
+                id: 34,
+                color: '#fff',
+                topic: 'Data & Information Management',
+                direction: 'right',
+                backgroundColor: '#fde5c6'
+              });
+          }
+          if(e.id == 50 ){
+            e.children.push({
+              id: 51,
+              color: '#fff',
+              topic: 'Virtual Systems & Services',
+              direction: 'right',
+              backgroundColor: '#a3cc5a'
+            },
+            {
+              id: 52,
+              color: '#fff',
+              topic: 'Intelligent System(AI)',
+              direction: 'right',
+              backgroundColor: '#a3cc5a'
+            },
+            {
+              id: 53,
+              color: '#fff',
+              topic: 'Internet of Things',
+              direction: 'right',
+              backgroundColor: '#a3cc5a'
+            },
+            {
+              id: 54,
+              color: '#fff',
+              topic: 'Parallel & Distributed Computing',
+              direction: 'right',
+              backgroundColor: '#a3cc5a'
+            },
+            {
+              id: 55,
+              color: '#fff',
+              topic: 'Computer Network',
+              direction: 'right',
+              backgroundColor: '#a3cc5a'
+            },
+            {
+              id: 56,
+              color: '#fff',
+              topic: 'Embedded System',
+              direction: 'right',
+              backgroundColor: '#a3cc5a'
+            },
+            {
+              id: 57,
+              color: '#fff',
+              topic: 'Integrated System Technology',
+              direction: 'right',
+              backgroundColor: '#a3cc5a'
+            },
+            {
+              id: 58,
+              color: '#fff',
+              topic: 'Platform Technology',
+              direction: 'right',
+              backgroundColor: '#a3cc5a'
+            },
+            {
+              id: 59,
+              color: '#fff',
+              topic: 'Security Technology & Implementation',
+              direction: 'right',
+              backgroundColor: '#a3cc5a'
+            });
+        }
+
+        if(e.id == 70 ){
+          e.children.push({
+            id: 71,
+            color: '#fff',
+            topic: 'Software Quality, Verification & Validation',
+            direction: 'right',
+            backgroundColor: '#beb66d'
+          },
+          {
+            id: 72,
+            color: '#fff',
+            topic: 'Software Process',
+            direction: 'right',
+            backgroundColor: '#beb66d'
+          },
+          {
+            id: 73,
+            color: '#fff',
+            topic: 'Software Modeling & Analysis',
+            direction: 'right',
+            backgroundColor: '#beb66d'
+          },
+          {
+            id: 74,
+            color: '#fff',
+            topic: 'Software Design',
+            direction: 'right',
+            backgroundColor: '#beb66d'
+          },
+          {
+            id: 75,
+            color: '#fff',
+            topic: 'Platform-Based Development',
+            direction: 'right',
+            backgroundColor: '#beb66d'
+          }
+        );
+      }
+
+        if(e.id == 80 ){
+          e.children.push({
+            id: 81,
+            color: '#fff',
+            topic: 'Graphics & Visualization',
+            direction: 'right',
+            backgroundColor: '#9bd1a2'
+          },
+          {
+            id: 82,
+            color: '#fff',
+            topic: 'Operation System',
+            direction: 'right',
+            backgroundColor: '#9bd1a2'
+          },
+          {
+            id: 83,
+            color: '#fff',
+            topic: 'Data Structure, Alogorithms & Complexity',
+            direction: 'right',
+            backgroundColor: '#9bd1a2'
+          },
+          {
+            id: 84,
+            color: '#fff',
+            topic: 'Programming Languages',
+            direction: 'right',
+            backgroundColor: '#9bd1a2'
+          },
+          {
+            id: 85,
+            color: '#fff',
+            topic: 'Programming Fundamentals',
+            direction: 'right',
+            backgroundColor: '#9bd1a2'
+          },
+          {
+            id: 86,
+            color: '#fff',
+            topic: 'Computer Systems Fundamentals',
+            direction: 'right',
+            backgroundColor: '#9bd1a2'
+          }
+        );
+      }
+
+      if(e.id == 100 ){
+        e.children.push({
+          id: 101,
+          color: '#fff',
+          topic: 'Architecture & Organization',
+          direction: 'right',
+          backgroundColor: '#e5957b'
+        },
+        {
+          id: 102,
+          color: '#fff',
+          topic: 'Digital Design',
+          direction: 'right',
+          backgroundColor: '#e5957b'
+        },
+        {
+          id: 103,
+          color: '#fff',
+          topic: 'Circuit & Electronics',
+          direction: 'right',
+          backgroundColor: '#e5957b'
+        },
+        {
+          id: 104,
+          color: '#fff',
+          topic: 'Signal Processing',
+          direction: 'right',
+          backgroundColor: '#e5957b'
+        }
+      );
+    }
+
           level2.push(e);
         }
       });
@@ -213,6 +448,7 @@ export class AppComponent {
       }
     };
 
+    debugger
     this.resetMindMap();
     this.mindMap._show(level2Mind);
     this.selectedNodes.length=0;
